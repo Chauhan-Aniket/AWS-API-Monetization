814845e6e9fb0e9ed5f467f9f09e9e24a79e2f55
Chauhan-Aniket <aniketchauhan2125@gmail.com>
2022-10-09 15:00:15 +0530

    modify: server GET response

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 const port = 5000;
+app.use(express.static(path.join(__dirname, "/src")));
 
 app.get("/", function (_req, res) {
-	res.send("Hello, World");
+	res.sendFile(path.join(__dirname, "index.html"));
+	res.sendFile(path.join(__dirname, "app.js"));
 });
 
 app.listen(port, () => {
