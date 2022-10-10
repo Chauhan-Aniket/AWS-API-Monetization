e2fe1a4040991e34871311eba8b04af9d9c0747b
Chauhan-Aniket <aniketchauhan2125@gmail.com>
2022-10-10 09:47:34 +0530

    create: register endpoint for APIkey params

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 require("dotenv").config();
-var bodyParser = require("body-parser");
+const bodyParser = require("body-parser");
 const moesif = require("moesif-nodejs");
 const Stripe = require("stripe");
 // npm install @aws-sdk/client-api-gateway
@@ -12,6 +12,9 @@ const {
 } = require("@aws-sdk/client-api-gateway");
 
 const jsonParser = bodyParser.json();
+const app = express();
+const port = 5000;
+app.use(express.static(path.join(__dirname, "/src")));
 
 const config = {
 	invokeUrl: process.env.AWS_INVOKE_URL,
@@ -31,9 +34,20 @@ const moesifMiddleware = moesif({
 
 app.use(moesifMiddleware);
 
-const app = express();
-const port = 5000;
-app.use(express.static(path.join(__dirname, "/src")));
+app.post("/register", jsonParser, async (req, res) => {
+	console.log(req.body);
+
+	// Generate API key
+	const params = {
+		name: req.body.email,
+		enabled: true,
+	};
+	const command = new CreateApiKeyCommand(params);
+	const response = await client.send(command);
+	const awsApiKeyId = response.id;
+	const awsApiKey = response.value;
+	console.log(response);
+});
 
 app.get("/", function (_req, res) {
 	res.sendFile(path.join(__dirname, "index.html"));
