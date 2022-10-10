90cd0d20b31f0689e98e4be777382a36902c6184
Chauhan-Aniket <aniketchauhan2125@gmail.com>
2022-10-10 09:31:14 +0530

    create: configuration of AWS, Stripe, Moesif

--- index.js
+++ index.js
@@ -1,5 +1,35 @@
 const express = require("express");
 const path = require("path");
+require("dotenv").config();
+var bodyParser = require("body-parser");
+const moesif = require("moesif-nodejs");
+const Stripe = require("stripe");
+// npm install @aws-sdk/client-api-gateway
+const {
+	APIGatewayClient,
+	CreateApiKeyCommand,
+	CreateUsagePlanKeyCommand,
+} = require("@aws-sdk/client-api-gateway");
+
+const jsonParser = bodyParser.json();
+
+const config = {
+	invokeUrl: process.env.AWS_INVOKE_URL,
+	region: process.env.AWS_REGION,
+	credentials: {
+		accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+		secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+	},
+};
+const client = new APIGatewayClient(config);
+
+const stripe = Stripe(process.env.STRIPE_KEY);
+
+const moesifMiddleware = moesif({
+	applicationId: process.env.MOESIF_APPLICATION_ID,
+});
+
+app.use(moesifMiddleware);
 
 const app = express();
 const port = 5000;
